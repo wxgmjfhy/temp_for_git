c3f5ec7c4531f466f15debdea486a884c44c5887
wxgmjfhy <3281412030@qq.com>
2025-01-15 01:12:43 +0800

    moban

--- temp.cpp
+++ temp.cpp
@@ -1,10 +1,20 @@
 #include <bits/stdc++.h>
+using namespace std;
+
+inline void solve() {
+
+}
 
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
-    std::cout << "Hello World!" << std::endl;
+    int t = 1;
+    cin >> t;
 
+    while (t--) {
+        solve();
+    }
+    
     return 0;
 }
